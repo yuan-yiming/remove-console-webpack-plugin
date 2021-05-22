5d52d5d6469733e69c62474d18de45ef9e2a95bc
yuan yiming <850319041@qq.com>
2021-05-22 23:11:29 +0800

    add document

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # remove-console-webpack-plugin
 A light webpack plugin to remove console in your JS code.
+
+## Example
+
+**In**
+
+```js
+console.log("log test"); // by default
+console.error("error test"); // need to configure
+```
+
+**Out**
+
+```js
+```
+
+## Installation
+```sh
+npm install remove-console-webpack-plugin --save-dev
+```
+
+## Usage
+```js
+// webpack.config.js
+const RemoveConsolePlugin = require('remove-console-webpack-plugin')
+
+// demo1: remove console.log by default
+module.exports = {
+  // other code ...
+  plugins: [
+    new RemoveConsolePlugin()
+  ]
+}
+
+// demo2: remove console.log, console.warn, console.error 
+module.exports = {
+  // other code ...
+  plugins: [
+    new RemoveConsolePlugin({ include: ['log', 'warn', 'error'] })
+  ]
+}
+
+// demo3: remove console.*
+module.exports = {
+  // other code ...
+  plugins: [
+    new RemoveConsolePlugin({ include: ['*'] })
+  ]
+}
+```
+
+
+## Options
+| option | description | default |
+| ------ | ----------- | ------- |
+| include | An array of console methods than you want to remove. | ['log']|
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
+/**
+ * desc: A light webpack plugin to remove console in your JS code.
+ * author: Yuamn-yiming
+ * date: 2021/05/21
+ */
 class RemoveConsoleWebpackPlugin {
   // 构造函数接受配置参数
   constructor(options) {
     let include = options && options.include;
-    let removed = ['log']; // 默认清除方法
+    let removed = ['log']; // 默认清除的方法
 
     if (include) {
       if (!Array.isArray(include)) {
         console.error('options.include must be an Array.');
       } else if (include.includes('*')) {
+        // 传入 * 表示清除所有 console 的方法
         removed = Object.keys(console).filter(fn => {
           return typeof console[fn] === 'function';
         })
       } else {
-        removed = include;
+        removed = include; // 根据传入配置覆盖
       }
     }
 
@@ -23,18 +29,25 @@ class RemoveConsoleWebpackPlugin {
   apply(compiler) {
     // js 资源代码处理函数
     let assetsHandler = (assets, compilation) => {
-      let includeStr = this.removed.reduce((a, b) => (a + '|' + b));
+      let removedStr = this.removed.reduce((a, b) => (a + '|' + b));
 
-      let re1 = RegExp(`(window\\.|)console\\.(${includeStr})\\(\\)`, 'g');
-      let re2 = RegExp(`(window\\.|)console\\.(${includeStr})\\(`, 'g');
+      let reDict = {
+        1: [RegExp(`\\.console\\.(${removedStr})\\(\\)`, 'g'), ''],
+        2: [RegExp(`\\.console\\.(${removedStr})\\(`, 'g'), ';('],
+        3: [RegExp(`console\\.(${removedStr})\\(\\)`, 'g'), ''],
+        4: [RegExp(`console\\.(${removedStr})\\(`, 'g'), '(']
+      }
 
       Object.entries(assets).forEach(([filename, source]) => {
         // 匹配js文件
         if (/\.js$/.test(filename)) {
           // 处理前文件内容
-          source = source.source();
-          // 处理后文件内容
-          let outputContent = source.replace(re1, '').replace(re2, '(');
+          let outputContent = source.source();
+
+          Object.keys(reDict).forEach(i => {
+            let [re, s] = reDict[i];
+            outputContent = outputContent.replace(re, s);
+          })
 
           compilation.assets[filename] = {
             // 返回文件内容
